1b4766a984de505e702700ff25f987973c3723db
txy <656877449@qq.com>
2020-10-30 16:42:32 +0800

    新增或修改

--- pages/store/store.js
+++ pages/store/store.js
@@ -16,24 +16,43 @@ Page({
     autoplay: true,
     interval: 8000,
     duration: 1000,
+    integral: 0,
     product_data: [
       {
         photo: "https://dss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1689053532,4230915864&fm=26&gp=0.jpg",
+        content: "iphone11",
+        remaining_amount: "0",
+        product_score: "99999999"
       },
       {
         photo: "https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=3238317745,514710292&fm=26&gp=0.jpg",
+        content: "Apple Watch",
+        remaining_amount: "0",
+        product_score: "99999999"
       },
       {
-        photo: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1603994765111&di=b4e9038443e9313eed9d1f8f2d196ab9&imgtype=0&src=http%3A%2F%2Fa1.att.hudong.com%2F05%2F00%2F01300000194285122188000535877.jpg",
+        photo: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1603974089565&di=aa4390823043f595d57d0505f5e2b7ca&imgtype=0&src=http%3A%2F%2Fimg.article.pchome.net%2F00%2F34%2F09%2F06%2Fpic_lib%2Fs960x639%2Fjmfj061s960x639.jpg",
+        content: "小米手环",
+        remaining_amount: "0",
+        product_score: "99999999"
       },
       {
-        photo: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1603994765106&di=67a3d92e88b42ba13023f742f20d427d&imgtype=0&src=http%3A%2F%2Fcdn2.image.apk.gfan.com%2Fasdf%2FPImages%2F2012%2F2%2F5%2F224702_219ba9e6b-4425-428f-b5e8-816d7aafd8cd.jpg",
+        photo: "https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=1938832210,427473212&fm=26&gp=0.jpg",
+        content: "小米电视",
+        remaining_amount: "0",
+        product_score: "99999999"
       },
       {
         photo: "https://dss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1689053532,4230915864&fm=26&gp=0.jpg",
+        content: "Air Pods",
+        remaining_amount: "0",
+        product_score: "99999999"
       },
       {
         photo: "https://dss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1689053532,4230915864&fm=26&gp=0.jpg",
+        content: "小米充电宝",
+        remaining_amount: "0",
+        product_score: "99999999"
       }
     ]
   },
