a9b91a0b90f1f7490b51370792e61716d96206e7
txy <656877449@qq.com>
2020-11-05 15:19:19 +0800

    新增或修改

--- pages/store/store.js
+++ pages/store/store.js
@@ -28,19 +28,19 @@ Page({
         photo: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1604503978681&di=47e990791fee03e7840da92778bef88c&imgtype=0&src=http%3A%2F%2Fimg.pconline.com.cn%2Fimages%2Fproduct%2F1093607%2F20189%2F14%2F15369099173839054_800.JPG",
         content: "Apple Watch Series 6",
         remaining_amount: "0",
-        product_score: "32000000"
+        product_score: "32999999"
       },
       {
         photo: "https://img.pconline.com.cn/images/product/1193/1193687/29383059a71cb1b6.jpg",
         content: "小米手环4",
         remaining_amount: "0",
-        product_score: "2300000"
+        product_score: "2355555"
       },
       {
         photo: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1604506322768&di=c87356ea20a5800b6f82ca9ca87f3a3f&imgtype=0&src=http%3A%2F%2Fc1.mifile.cn%2Ff%2Fi%2F2014%2Fcn%2Fmitv40%2Fmitv_summary_header_1.jpg",
         content: "小米电视3s",
         remaining_amount: "0",
-        product_score: "7000000"
+        product_score: "6999999"
       },
       {
         photo: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1604506520395&di=eee54763c860dad2dbcde3a14f978fd6&imgtype=0&src=http%3A%2F%2Ffiles.nowre.com%2Farticles%2F2019%2F10%2FMWP22.jpeg",
@@ -52,7 +52,7 @@ Page({
         photo: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1604504159569&di=19221e616859a9e45a937d6d5223e294&imgtype=0&src=http%3A%2F%2Fimg.pconline.com.cn%2Fimages%2Fproduct%2F1295%2F1295848%2F2_sn8.jpg",
         content: "iMac",
         remaining_amount: "0",
-        product_score: "150000000"
+        product_score: "158888888"
       }
     ]
   },
