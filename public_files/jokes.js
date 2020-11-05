70ba291ff7d2bd39e7dfd5cdfd3e65a8a11f64c5
txy <656877449@qq.com>
2020-11-05 17:59:48 +0800

    新增或修改

--- public_files/jokes.js
+++ public_files/jokes.js
@@ -2,6 +2,166 @@
 let jokes_data = [
   {
     name: "搞笑段子精选", 
+    content: "孩子，你撞墙是迟早的事儿！",
+    icon: "https://dss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1593106255,4245861836&fm=26&gp=0.jpg",
+    photo: "https://p6-tt-ipv6.byteimg.com/origin/pgc-image/7630b304085445d29e5074ecf1d0b64e",
+    release_time: "发布于 20/11/05 17:14",
+    id: 40
+  },
+  {
+    name: "搞笑段子精选", 
+    content: "狗子：主人让我给他看车，可是我太困了",
+    icon: "https://dss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1593106255,4245861836&fm=26&gp=0.jpg",
+    photo: "https://p1-tt-ipv6.byteimg.com/origin/pgc-image/b2f1a2c62e9145fab7f0399ce4900adc",
+    release_time: "发布于 20/11/05 17:14",
+    id: 39
+  },
+  {
+    name: "搞笑段子精选", 
+    content: "不想加班的你是这样吗",
+    icon: "https://dss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1593106255,4245861836&fm=26&gp=0.jpg",
+    photo: "http://i3.xiaohua.fd.zol-img.com.cn/t_s600x5000/g5/M00/0C/02/ChMkJldkmxeIK_QaAAObPI9HjiUAASt-gB51rAAA5tU029.gif",
+    release_time: "发布于 20/11/05 17:14",
+    id: 38
+  },
+  {
+    name: "搞笑段子精选", 
+    content: "有个这样逗逼的爸比，骚年也是操碎了心！",
+    icon: "https://dss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1593106255,4245861836&fm=26&gp=0.jpg",
+    photo: "http://i3.xiaohua.fd.zol-img.com.cn/t_s600x5000/g5/M00/0E/0A/ChMkJ1drXEOISp0hAFhvU8uvdD4AAS34gB2JfYAWG9r833.gif",
+    release_time: "发布于 20/11/05 17:14",
+    id: 37
+  },
+  {
+    name: "搞笑段子精选", 
+    content: "第一次见这么主动的鱼",
+    icon: "https://dss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1593106255,4245861836&fm=26&gp=0.jpg",
+    photo: "http://i0.xiaohua.fd.zol-img.com.cn/t_s600x5000/g5/M00/02/02/ChMkJld0iGaIX8QyABNhi-n-GYcAATF0gB2gxIAE2Gj686.gif",
+    release_time: "发布于 20/11/05 17:14",
+    id: 36
+  },
+  {
+    name: "搞笑段子精选", 
+    content: "挖掘机从娃娃抓起",
+    icon: "https://dss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1593106255,4245861836&fm=26&gp=0.jpg",
+    photo: "http://i3.xiaohua.fd.zol-img.com.cn/t_s600x5000/g5/M00/03/0A/ChMkJ1ffmq2IHK9yAB66OHZ3Do8AAWLmwGebcEAHrpQ039.gif",
+    release_time: "发布于 20/11/05 17:14",
+    id: 35
+  },
+  {
+    name: "搞笑段子精选", 
+    content: "仿佛对生活失去热情的你",
+    icon: "https://dss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1593106255,4245861836&fm=26&gp=0.jpg",
+    photo: "http://i2.xiaohua.fd.zol-img.com.cn/t_s600x5000/g5/M00/0A/0A/ChMkJlxqZXiICj7FAAByWrYFa4YAAu9dQAjMU4AAHJy598.jpg",
+    release_time: "发布于 20/11/05 17:14",
+    id: 34
+  },
+  {
+    name: "搞笑段子精选", 
+    content: "秒怂",
+    icon: "https://dss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1593106255,4245861836&fm=26&gp=0.jpg",
+    photo: "http://i5.xiaohua.fd.zol-img.com.cn/t_s600x5000/g2/M00/08/04/ChMlWlxzjWeIZ6fqAEZuISbZqGMAAIa_QFInkwARm45245.gif",
+    release_time: "发布于 20/11/05 17:14",
+    id: 33
+  },
+  {
+    name: "搞笑段子精选", 
+    content: "高手在民间",
+    icon: "https://dss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1593106255,4245861836&fm=26&gp=0.jpg",
+    photo: "http://i4.xiaohua.fd.zol-img.com.cn/t_s600x5000/g2/M00/0A/0E/ChMlWlx8kVyIT1oDABW36v0GGU0AAIllACZkgwAFbgC974.gif",
+    release_time: "发布于 20/11/05 17:14",
+    id: 32
+  },
+  {
+    name: "搞笑段子精选", 
+    content: "好大的雪堆啊",
+    icon: "https://dss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1593106255,4245861836&fm=26&gp=0.jpg",
+    photo: "http://i1.xiaohua.fd.zol-img.com.cn/t_s600x5000/g2/M00/0D/03/ChMlWVyGJPyIKSFqAECQzWs-RUEAAIu4AHLrskAQJDl607.gif",
+    release_time: "发布于 20/11/05 17:14",
+    id: 31
+  },
+  {
+    name: "搞笑段子精选", 
+    content: "厉害啊，这个箱子成精了",
+    icon: "https://dss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1593106255,4245861836&fm=26&gp=0.jpg",
+    photo: "http://abc.gaoxiaozi.com/gif/202006/0cab46f4b517571dd6c082253056eede.gif",
+    release_time: "发布于 20/11/05 17:14",
+    id: 30
+  },
+  {
+    name: "搞笑段子精选", 
+    content: "两个恶棍找老人家麻烦，剧情反转",
+    icon: "https://dss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1593106255,4245861836&fm=26&gp=0.jpg",
+    photo: "http://wx1.sinaimg.cn/large/d54a1fa7ly1gfqqd4b8jsg20cc06xqv7.gif",
+    release_time: "发布于 20/11/05 17:14",
+    id: 29
+  },
+  {
+    name: "搞笑段子精选", 
+    content: "遥控捅马蜂窝",
+    icon: "https://dss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1593106255,4245861836&fm=26&gp=0.jpg",
+    photo: "http://i2.xiaohua.fd.zol-img.com.cn/t_s600x5000/g2/M00/0D/03/ChMlWVyGJrKIDBlNABWNr2k5xREAAIu4wEHLTIAFY3H362.gif",
+    release_time: "发布于 20/11/05 17:14",
+    id: 28
+  },
+  {
+    name: "搞笑段子精选", 
+    content: "雨中惊险刺激的一幕",
+    icon: "https://dss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1593106255,4245861836&fm=26&gp=0.jpg",
+    photo: "http://i4.xiaohua.fd.zol-img.com.cn/t_s600x5000/g3/M01/00/07/Cg-4V1I2n-qIP88mAAGKmMAc4IAAALq9QAbXYcAAYqw824.jpg",
+    release_time: "发布于 20/11/05 17:14",
+    id: 27
+  },
+  {
+    name: "搞笑段子精选", 
+    content: "据说这道题考倒一片平时翘课的大学生",
+    icon: "https://dss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1593106255,4245861836&fm=26&gp=0.jpg",
+    photo: "http://i1.xiaohua.fd.zol-img.com.cn/t_s600x5000/g3/M01/00/07/Cg-4V1I2n-2ICN26AADHROmjRZoAALq9QBTtS8AAMdc331.jpg",
+    release_time: "发布于 20/11/05 17:14",
+    id: 26
+  },
+  {
+    name: "搞笑段子精选", 
+    content: "这家人玩QQ农场入魔了",
+    icon: "https://dss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1593106255,4245861836&fm=26&gp=0.jpg",
+    photo: "http://i0.xiaohua.fd.zol-img.com.cn/t_s600x5000/g3/M01/00/07/Cg-4WFI2n--IaDbUAADKM86l9yMAALq9QB2-H8AAMpL080.jpg",
+    release_time: "发布于 20/11/05 17:14",
+    id: 25
+  },
+  {
+    name: "搞笑段子精选", 
+    content: "再大的鸟都装得下",
+    icon: "https://dss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1593106255,4245861836&fm=26&gp=0.jpg",
+    photo: "http://i0.xiaohua.fd.zol-img.com.cn/t_s600x5000/g4/M0B/0B/00/Cg-4WVI2n_uIVG6_AAGZEcWoDecAALrHQOoxjQAAZkp166.jpg",
+    release_time: "发布于 20/11/05 17:14",
+    id: 24
+  },
+  {
+    name: "搞笑段子精选", 
+    content: "问：见过猫咪流鼻涕吗？答：还真没见过！",
+    icon: "https://dss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1593106255,4245861836&fm=26&gp=0.jpg",
+    photo: "http://i0.xiaohua.fd.zol-img.com.cn/t_s600x5000/g4/M0B/0B/00/Cg-4WVI2n_-INr3FAADfDJbALJ4AALrHQPTsy0AAN8k440.jpg",
+    release_time: "发布于 20/11/05 17:14",
+    id: 23
+  },
+  {
+    name: "搞笑段子精选", 
+    content: "导演你能认真点儿吗",
+    icon: "https://dss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1593106255,4245861836&fm=26&gp=0.jpg",
+    photo: "http://i5.xiaohua.fd.zol-img.com.cn/t_s600x5000/g2/M00/06/07/ChMlWlyq_pKIfD5WABjHxz2KxPoAAJTtgHk_qkAGMff975.gif",
+    release_time: "发布于 20/11/05 17:14",
+    id: 22
+  },
+  {
+    name: "搞笑段子精选", 
+    content: "和爸爸一起带卡子的时光",
+    icon: "https://dss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1593106255,4245861836&fm=26&gp=0.jpg",
+    photo: "https://www.biedoul.com/Uploads/Images/57/5717358649ade.jpg",
+    release_time: "发布于 20/11/05 17:14",
+    id: 21
+  },
+  {
+    name: "搞笑段子精选", 
     content: "真搞不懂你们这些设计师",
     icon: "https://dss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1906469856,4113625838&fm=26&gp=0.jpg",
     photo: "https://www.biedoul.com/Uploads/Images/81/5716ee937f6ee.jpg",
